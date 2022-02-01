4663c028b27581435c8235a407d7cf9324181db4
Aliaksandr Astrouski <aostrovsky87@gmail.com>
2022-02-01 15:28:34 +0300

    feat: add download function and final changes

--- eco-sounds/script.js
+++ eco-sounds/script.js
@@ -1,4 +1,18 @@
-console.log('Поехали!');
+let requirements = `Итоговая оценка: 70/70.
+1. Вёрстка +10
+  - есть не меньше пяти интерактивных элементов, с которыми пользователи могут взаимодействовать. Изменение внешнего вида самого элемента и состояния курсора при наведении, плавные анимации +5
+  - в футере приложения есть ссылка на гитхаб автора приложения, год создания приложения, логотип курса со ссылкой на курс +5
+2. При кликах по интерактивным элементам меняется изображение +10
+3. При кликах по интерактивным элементам меняется звук +10
+4. Активный в данный момент интерактивный элемент выделяется стилем +10
+5. Кнопка Play/Pause +20
+  - есть кнопка Play/Pause, при клике по которой можно запустить или остановить проигрывание звука +10
+  - внешний вид и функционал кнопки Play/Pause изменяется в зависимости от того, проигрывается ли в данный момент звук +10
+6. Дополнительный - не предусмотренный в задании - функционал, улучшающий качество приложения +10
+  - Кнопка Скачать, по нажатию на которую можно скачать звук, выбранный в текущий момент
+`;
+
+console.log(requirements);
 
 let isPlay = false;
 const button = document.querySelector('.play');
@@ -6,9 +20,20 @@ const audio = new Audio();
 audio.src = `assets/audio/forest.mp3`
 const image = new Image();
 const main = document.querySelector('.main');
+const links = document.querySelectorAll('.nav-item');
+const sounds = document.querySelector('.nav-list');
+const download = document.querySelector('.download');
 
 const setAudio = (source) => audio.src = `assets/audio/${source}.mp3`;
+
 const setImage = (source) => main.style.backgroundImage = `url('assets/img/${source}.webp')`;
+
+const setDownload = (source) => download.href = `assets/audio/${source}.mp3`;
+
+const setActive = (elem) => {
+    links.forEach(el => el.classList.remove('active'));
+    elem.classList.add('active');
+}
 
 const playAudio = () => {
     if (!isPlay) {
@@ -26,8 +51,6 @@ const toggleBtn = () => {
     playAudio();
 }
 
-button.addEventListener('click', toggleBtn);
-
 const selectAudio = (event) => {
     isPlay = false;
     if (event.target.dataset.audio) {
@@ -36,10 +59,11 @@ const selectAudio = (event) => {
         }
         setAudio(event.target.dataset.audio);
         setImage(event.target.dataset.audio);
+        setDownload(event.target.dataset.audio);
+        setActive(event.target);
         toggleBtn();
     }
 }
 
-const sounds = document.querySelector('.nav-list');
-
+button.addEventListener('click', toggleBtn);
 sounds.addEventListener('click', selectAudio);
