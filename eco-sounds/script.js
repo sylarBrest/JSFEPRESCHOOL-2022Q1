796dfe6907695a7320e232b3f54ccb289694ed78
Aliaksandr Astrouski <aostrovsky87@gmail.com>
2022-02-01 13:23:14 +0300

    feat: add play function

--- eco-sounds/script.js
+++ eco-sounds/script.js
@@ -1,1 +1,25 @@
 console.log('Поехали!');
+
+let isPlay = false;
+const button = document.querySelector('.play');
+const audio = new Audio();
+
+const playAudio = () => {
+    audio.src = `assets/audio/forest.mp3`
+    if (!isPlay) {
+        audio.currentTime = 0;
+        audio.play();
+        isPlay = true;
+    }
+    else {
+        audio.pause();
+        isPlay = false;
+    }
+}
+
+const toggleBtn = () => {
+    button.classList.toggle('pause');
+    playAudio();
+}
+
+button.addEventListener('click', toggleBtn);
