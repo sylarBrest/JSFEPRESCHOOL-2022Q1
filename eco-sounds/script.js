d7bbd19891faf98883835bd315c0ddfac0e98b9d
Aliaksandr Astrouski <aostrovsky87@gmail.com>
2022-02-01 14:44:45 +0300

    feat: add change audio and image function

--- eco-sounds/script.js
+++ eco-sounds/script.js
@@ -3,18 +3,22 @@ console.log('Поехали!');
 let isPlay = false;
 const button = document.querySelector('.play');
 const audio = new Audio();
+audio.src = `assets/audio/forest.mp3`
+const image = new Image();
+const main = document.querySelector('.main');
+
+const setAudio = (source) => audio.src = `assets/audio/${source}.mp3`;
+const setImage = (source) => main.style.backgroundImage = `url('assets/img/${source}.webp')`;
 
 const playAudio = () => {
-    audio.src = `assets/audio/forest.mp3`
     if (!isPlay) {
         audio.currentTime = 0;
         audio.play();
-        isPlay = true;
     }
     else {
         audio.pause();
-        isPlay = false;
     }
+    isPlay = !isPlay;
 }
 
 const toggleBtn = () => {
@@ -23,3 +27,19 @@ const toggleBtn = () => {
 }
 
 button.addEventListener('click', toggleBtn);
+
+const selectAudio = (event) => {
+    isPlay = false;
+    if (event.target.dataset.audio) {
+        if (button.classList.contains('pause')) {
+            button.classList.remove('pause');
+        }
+        setAudio(event.target.dataset.audio);
+        setImage(event.target.dataset.audio);
+        toggleBtn();
+    }
+}
+
+const sounds = document.querySelector('.nav-list');
+
+sounds.addEventListener('click', selectAudio);
