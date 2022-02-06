2f40ae641050784b2496ac4c5bc0ba8e8d52d8ce
Aliaksandr Astrouski <aostrovsky87@gmail.com>
2022-02-07 00:35:57 +0300

    feat: get quotes from api and json

--- random-jokes/script.js
+++ random-jokes/script.js
@@ -1,4 +1,4 @@
-let requirements = `Итоговая оценка: 10/60.
+let requirements = `Итоговая оценка: 40/60.
 1. Вёрстка +10
   - на странице есть цитата и кнопка для смены цитаты +5
   - в футере приложения есть ссылка на гитхаб автора приложения, год создания приложения, логотип курса со ссылкой на курс +5
@@ -12,15 +12,31 @@ let requirements = `Итоговая оценка: 10/60.
 console.log(requirements);
 
 const p = document.querySelector('.quote');
+const button = document.querySelector('.button');
 
 const url = 'https://type.fit/api/quotes';
 
 async function getData() {
-  const res = await fetch(url);
-  const data = await res.json();
-  p.textContent = data[0].text;
+    const res = await fetch(url);
+    const data = await res.json();
+    showData(data);
+}
+
+function showData(data) {
+    const rand = Math.floor(Math.random() * data.length);
+    p.textContent = data[rand].text;
 }
 
 getData();
 
+async function getQuotes() {  
+    const quotes = './assets/quotes.json';
+    const res = await fetch(quotes);
+    const data = await res.json(); 
+    showData(data);
+}
 
+//getQuotes();
+
+
+button.addEventListener('click', getData);
