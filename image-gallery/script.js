1806026c8486bcb154b39a36f019f91047c93a53
Aliaksandr Astrouski <aostrovsky87@gmail.com>
2022-02-08 00:14:04 +0300

    feat: add images from flickr api

--- image-gallery/script.js
+++ image-gallery/script.js
@@ -2,3 +2,25 @@ let requirements = `Итоговая оценка: 0/60.
 `;
 
 console.log(requirements);
+
+const galleryContainer = document.querySelector('.main-container');
+
+const url = 'https://www.flickr.com/services/rest/?method=flickr.photos.search&per_page=30&api_key=9a4f86e3e3903203825e1c780bdec681&tags=spring,nature&tag_mode=all&extras=url_m&format=json&nojsoncallback=1';
+
+async function getData() {
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data.photos.photo[0].url_m);
+    console.log(data.photos.photo);
+    showData(data.photos.photo);
+}
+
+getData();
+
+function showData(data) {
+    data.forEach(element => {
+        const img = `<img class="gallery-img" src=${element.url_m} alt="image">`;
+        galleryContainer.insertAdjacentHTML('beforeend', img);
+          
+    });
+}
