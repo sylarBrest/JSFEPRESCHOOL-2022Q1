29d7ae5dbff6f9817fdf84955c892af2f0210ca1
Aliaksandr Astrouski <aostrovsky87@gmail.com>
2022-02-09 00:15:14 +0300

    fix: fix search

--- image-gallery/script.js
+++ image-gallery/script.js
@@ -1,15 +1,15 @@
-let requirements = `Итоговая оценка: 35/60.
+let requirements = `Итоговая оценка: 60/60.
 1. Вёрстка +10
   - на странице есть несколько фото и строка поиска +5
   - в футере приложения есть ссылка на гитхаб автора приложения, год создания приложения, логотип курса со ссылкой на курс +5
 2. При загрузке приложения на странице отображаются полученные от API изображения +10
-x3. Если в поле поиска ввести слово и отправить поисковый запрос, на странице отобразятся изображения соответствующей тематики, если такие данные предоставляет API +10
+3. Если в поле поиска ввести слово и отправить поисковый запрос, на странице отобразятся изображения соответствующей тематики, если такие данные предоставляет API +10
 4. Поиск +30
-  -x при открытии приложения курсор находится в поле ввода +5
+  - при открытии приложения курсор находится в поле ввода +5
   - есть placeholder +5
   - автозаполнение поля ввода отключено (нет выпадающего списка с предыдущими запросами) +5
-  -x поисковый запрос можно отправить нажатием клавиши Enter +5
-  -x после отправки поискового запроса и отображения результатов поиска, поисковый запрос продолжает отображаться в поле ввода +5
+  - поисковый запрос можно отправить нажатием клавиши Enter +5
+  - после отправки поискового запроса и отображения результатов поиска, поисковый запрос продолжает отображаться в поле ввода +5
   - в поле ввода есть крестик при клике по которому поисковый запрос из поля ввода удаляется и отображается placeholder +5
 `;
 
@@ -17,20 +17,18 @@ console.log(requirements);
 
 const galleryContainer = document.querySelector('.main-container');
 const searchInput = document.querySelector('.search-input');
-const searchButton = document.querySelector('.search-button');
 const searchForm = document.querySelector('.search-form');
 
 let query = 'ferrari';
 const url = `https://api.unsplash.com/search/photos?query=${query}&per_page=30&orientation=landscape&client_id=9NT5CyTGxjhPmBFIRFIoFUsEr1d_RPe-SA3AU9SgBZ0`;
 
-async function getData() {
+async function getData(url) {
     const res = await fetch(url);
     const data = await res.json();
-//    console.log(data.results);
     showData(data.results);
 }
 
-getData();
+getData(url);
 
 function showData(data) {
     data.forEach(element => {
@@ -39,16 +37,24 @@ function showData(data) {
     });
 }
 
-const find = () => {
-    query = searchInput.value;
+async function getNewData(url) {
+    const res = await fetch(url);
+    const data = await res.json();
+    showData(data.results);
 }
 
-const search = (event) => {
+function deleteData() {
+    const imgs = galleryContainer.querySelectorAll('.gallery-img');
+    imgs.forEach(element => galleryContainer.removeChild(element));
+}
+
+function search (event) {
     event.preventDefault();
-    console.log(query);
-    console.log(getData());
+    const query = searchInput.value;
+    const url = `https://api.unsplash.com/search/photos?query=${query}&per_page=30&orientation=landscape&client_id=9NT5CyTGxjhPmBFIRFIoFUsEr1d_RPe-SA3AU9SgBZ0`;
+    deleteData();
+    getNewData(url);
 }
 
-//window.addEventListener('load', search);
-searchInput.addEventListener('input', find);
+window.addEventListener('load', () => searchInput.focus());
 searchForm.addEventListener('submit', search);
