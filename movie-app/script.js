51379d146be41a8a4bcb05bfd75e52f0e9a3ac0d
Aliaksandr Astrouski <aostrovsky87@gmail.com>
2022-02-09 13:22:43 +0300

    feat: add search request and movie-cards

--- movie-app/script.js
+++ movie-app/script.js
@@ -1,9 +1,9 @@
-let requirements = `Итоговая оценка: 65/65.
+let requirements = `Итоговая оценка: 70/70.
 1. Вёрстка +10
-  - на странице есть несколько фото и строка поиска +5
+  - на странице есть несколько карточек фильмов и строка поиска. На каждой карточке фильма есть постер и название фильма +5
   - в футере приложения есть ссылка на гитхаб автора приложения, год создания приложения, логотип курса со ссылкой на курс +5
-2. При загрузке приложения на странице отображаются полученные от API изображения +10
-3. Если в поле поиска ввести слово и отправить поисковый запрос, на странице отобразятся изображения соответствующей тематики, если такие данные предоставляет API +10
+2. При загрузке приложения на странице отображаются карточки поппулярных фильмов с полученными от API данными +10
+3. Если в поле поиска ввести слово и отправить поисковый запрос, на странице отобразятся карточки фильмов, в названиях которых есть это слово (или фраза), если такие данные предоставляет API +10
 4. Поиск +30
   - при открытии приложения курсор находится в поле ввода +5
   - есть placeholder +5
@@ -11,24 +11,26 @@ let requirements = `Итоговая оценка: 65/65.
   - поисковый запрос можно отправить нажатием клавиши Enter +5
   - после отправки поискового запроса и отображения результатов поиска, поисковый запрос продолжает отображаться в поле ввода +5
   - в поле ввода есть крестик при клике по которому поисковый запрос из поля ввода удаляется и отображается placeholder +5
-5. Дополнительный функционал +5
-  - если не находит изображений по запросу, то выводится надпись "No results found..." +1
-  - изображения сохраняют свои пропорции +4
+5. Дополнительный не предусмотренный в задании функционал, улучшающий качество приложения +10
+  - наличие на карточке фильма его описания и рейтинга на TMDB +2
+  - если не находит фильмов по запросу, то выводится надпись "No results found..." +2
+  - постеры фильмов сохраняют свои пропорции +2
+  - если постер не найден, то выводится замещающий постер +2
+  - если нажать на лого, то загрузятся популярные фильмы +2
 `;
 
 console.log(requirements);
 
-const galleryContainer = document.querySelector('.main-container');
+const returnMain = document.querySelector('.logo');
+const movieContainer = document.querySelector('.main-container');
 const searchForm = document.querySelector('.search-form');
 const searchInput = searchForm.querySelector('.search-input');
 
-let query = '';
-const url = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc${query}&api_key=3c141559256248f854d2665760baedf6`;
+const url = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3c141559256248f854d2665760baedf6`;
 
 async function getData(uri) {
     const res = await fetch(uri);
     const data = await res.json();
-    console.log(data);
     showData(data.results);
 }
 
@@ -37,34 +39,42 @@ getData(url);
 function showData(data) {
     if (data.length) {
         data.forEach(element => {
-
-            const img = `<img class="gallery-img" src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2${element.poster_path}" alt="image">`;
-            galleryContainer.insertAdjacentHTML('beforeend', img);
+            const posterSrc = (element.poster_path) ? `https://www.themoviedb.org/t/p/w600_and_h900_bestv2${element.poster_path}` : 'assets/img/not-found.webp'; 
+            const divMovie = `<div class="movie-card">
+                <img class="movie-poster" src="${posterSrc}" alt="image">
+                <div class="movie-info">
+                    <p class="movie-name">${element.title}</p>
+                    <p class="movie-rating">${element.vote_average}</p>
+                </div>
+            <p class="movie-desc">${element.overview}</p>
+            </div>
+            `;
+            movieContainer.insertAdjacentHTML('beforeend', divMovie);
         });
     }
     else {
         const p = `<p class="no-results">No results found...</p>`;
-        galleryContainer.insertAdjacentHTML('afterbegin', p);
+        movieContainer.insertAdjacentHTML('afterbegin', p);
     }
 }
 
 function deleteData() {
     const noResults = document.querySelector('.no-results');
     if (noResults) {
-        galleryContainer.removeChild(noResults);
+        movieContainer.removeChild(noResults);
     }
-    const imgs = galleryContainer.querySelectorAll('.gallery-img');
-    imgs.forEach(element => galleryContainer.removeChild(element));
+    const imgs = movieContainer.querySelectorAll('.movie-card');
+    imgs.forEach(element => movieContainer.removeChild(element));
 }
 
 function search (event) {
     event.preventDefault();
-    const query = `${searchInput.value}`;
+    const query = encodeURI(searchInput.value);
     const url = `https://api.themoviedb.org/3/search/movie?query=${query}&api_key=3c141559256248f854d2665760baedf6`;
-    console.log(url);
     deleteData();
     getData(url);
 }
 
 window.addEventListener('load', () => searchInput.focus());
+returnMain.addEventListener('click', () => window.location.reload());
 searchForm.addEventListener('submit', search);
