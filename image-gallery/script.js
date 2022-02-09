96f9f804aadd4ac1c7d2cfea51b369f7d3f5c05e
Aliaksandr Astrouski <aostrovsky87@gmail.com>
2022-02-09 09:41:11 +0300

    refactor: add media queries and styling images

--- image-gallery/script.js
+++ image-gallery/script.js
@@ -1,4 +1,4 @@
-let requirements = `Итоговая оценка: 60/60.
+let requirements = `Итоговая оценка: 65/65.
 1. Вёрстка +10
   - на странице есть несколько фото и строка поиска +5
   - в футере приложения есть ссылка на гитхаб автора приложения, год создания приложения, логотип курса со ссылкой на курс +5
@@ -11,13 +11,16 @@ let requirements = `Итоговая оценка: 60/60.
   - поисковый запрос можно отправить нажатием клавиши Enter +5
   - после отправки поискового запроса и отображения результатов поиска, поисковый запрос продолжает отображаться в поле ввода +5
   - в поле ввода есть крестик при клике по которому поисковый запрос из поля ввода удаляется и отображается placeholder +5
+5. Дополнительный функционал +5
+  - если не находит изображений по запросу, то выводится надпись "No results found..." +1
+  - изображения сохраняют свои пропорции +4
 `;
 
 console.log(requirements);
 
 const galleryContainer = document.querySelector('.main-container');
-const searchInput = document.querySelector('.search-input');
 const searchForm = document.querySelector('.search-form');
+const searchInput = searchForm.querySelector('.search-input');
 
 let query = 'ferrari';
 const url = `https://api.unsplash.com/search/photos?query=${query}&per_page=30&orientation=landscape&client_id=9NT5CyTGxjhPmBFIRFIoFUsEr1d_RPe-SA3AU9SgBZ0`;
@@ -31,19 +34,23 @@ async function getData(url) {
 getData(url);
 
 function showData(data) {
-    data.forEach(element => {
-        const img = `<img class="gallery-img" src=${element.urls.regular} alt="image">`;
-        galleryContainer.insertAdjacentHTML('beforeend', img);
-    });
-}
-
-async function getNewData(url) {
-    const res = await fetch(url);
-    const data = await res.json();
-    showData(data.results);
+    if (data.length) {
+        data.forEach(element => {
+            const img = `<img class="gallery-img" src=${element.urls.regular} alt="image">`;
+            galleryContainer.insertAdjacentHTML('beforeend', img);
+        });
+    }
+    else {
+        const p = `<p class="no-results">No results found...</p>`;
+        galleryContainer.insertAdjacentHTML('afterbegin', p);
+    }
 }
 
 function deleteData() {
+    const noResults = document.querySelector('.no-results');
+    if (noResults) {
+        galleryContainer.removeChild(noResults);
+    }
     const imgs = galleryContainer.querySelectorAll('.gallery-img');
     imgs.forEach(element => galleryContainer.removeChild(element));
 }
@@ -53,7 +60,7 @@ function search (event) {
     const query = searchInput.value;
     const url = `https://api.unsplash.com/search/photos?query=${query}&per_page=30&orientation=landscape&client_id=9NT5CyTGxjhPmBFIRFIoFUsEr1d_RPe-SA3AU9SgBZ0`;
     deleteData();
-    getNewData(url);
+    getData(url);
 }
 
 window.addEventListener('load', () => searchInput.focus());
