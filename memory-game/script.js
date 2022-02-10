00f12d638eb2fc0298a4dd229c4d0bd3792dfcbe
Aliaksandr Astrouski <aostrovsky87@gmail.com>
2022-02-11 00:17:09 +0300

    feat: add records save to localstorage

--- memory-game/script.js
+++ memory-game/script.js
@@ -1,11 +1,11 @@
-let requirements = `Итоговая оценка: 50/60.
+let requirements = `Итоговая оценка: 60/60.
 1. Вёрстка +10
   - реализован интерфейс игры +5
   - в футере приложения есть ссылка на гитхаб автора приложения, год создания приложения, логотип курса со ссылкой на курс +5
 2. Логика игры. Карточки, по которым кликнул игрок, переворачиваются согласно правилам игры +10
 3. Игра завершается, когда открыты все карточки +10
 4. По окончанию игры выводится её результат - количество ходов, которые понадобились для завершения игры +10
-x5. Результаты последних 10 игр сохраняются в local storage. Есть таблица рекордов, в которой сохраняются результаты предыдущих 10 игр +10
+5. Результаты последних 10 игр сохраняются в local storage. Есть таблица рекордов, в которой сохраняются результаты предыдущих 10 игр +10
 6. По клику на карточку – она переворачивается плавно, если пара не совпадает – обе карточки так же плавно переварачиваются рубашкой вверх +10
 x7. Очень высокое качество оформления приложения и/или дополнительный не предусмотренный в задании функционал, улучшающий качество приложения +10
   -x высокое качество оформления приложения предполагает собственное оригинальное оформление равное или отличающееся в лучшую сторону по сравнению с демо
@@ -36,6 +36,19 @@ let isLockedBoard = false;
 let firstCard, secondCard;
 let moves = 0;
 let unFlipPairs = Math.floor((rows * cols) / 2);
+let recordTable = [/* 1, 34, 45, 44, 55, 45, 67, 7, 78, 90 */];
+
+const setLocalStorage = () => localStorage.setItem('records', JSON.stringify(recordTable));
+
+const getLocalStorage = () => {
+  if (localStorage.getItem('records')) {
+    recordTable = JSON.parse(localStorage.getItem('records'));
+  }
+  fillRecordTable(recordTable);
+}
+
+window.addEventListener('load', getLocalStorage);
+window.addEventListener('beforeunload', setLocalStorage);
 
 const memoryCards = gameBoard.querySelectorAll('.memory-card');
 memoryCards.forEach(card => card.addEventListener('click', flipCard));
@@ -73,11 +86,32 @@ const disableMatchedCards = () => {
 const gameOver = document.querySelector('.game-over');
 const showModal = () => gameOver.classList.add('show');
 
+const writeRecord = () => {
+  recordTable.push(moves);
+  if (recordTable.length > 10) {
+    recordTable.shift();
+  }
+}
+
+const tableRecord = document.querySelector('.record-table');
+
+const fillRecordTable = table => {
+  for (let record of table) {
+    const pRecord = `<p class="record-line">${record} moves</p>`;
+    tableRecord.insertAdjacentHTML('beforeend', pRecord);
+  }
+}
+
+const tableRecordsButton = document.querySelector('.table-of-records');
+
+tableRecordsButton.addEventListener('click', () => tableRecord.classList.toggle('open'));
+
 const showGameOver = () => {
   const okButton = gameOver.querySelector('.ok');
   const pMoves = gameOver.querySelector('.moves');
   pMoves.textContent = `You make ${moves} moves`;
   showModal();
+  writeRecord();
   okButton.addEventListener('click', newGame);
 }
 
