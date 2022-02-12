9b0398b86d4ee52788b34efb10fda12715953e87
Aliaksandr Astrouski <aostrovsky87@gmail.com>
2022-02-13 00:18:46 +0300

    feat: add game logic

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,10 +1,10 @@
-let requirements = `Итоговая оценка: 30/60.
+let requirements = `Итоговая оценка: 50/60.
 1. Вёрстка +10
   - реализован интерфейс игры +5
   - в футере приложения есть ссылка на гитхаб автора приложения, год создания приложения, логотип курса со ссылкой на курс +5
 2. При кликах по игровому полю по очереди отображаются крестики и нолики. Первая фигура всегда крестик +10
-x3. Игра завершается, когда три фигуры выстроились в ряд по вертикали, горизонтали или диагонали +10
-x4. По окончанию игры выводится её результат - выигравшая фигура и количество ходов от начала игры до её завершения +10
+3. Игра завершается, когда три фигуры выстроились в ряд по вертикали, горизонтали или диагонали +10
+4. По окончанию игры выводится её результат - выигравшая фигура и количество ходов от начала игры до её завершения +10
 5. Результаты последних 10 игр сохраняются в local storage. Есть таблица рекордов, в которой отображаются результаты предыдущих 10 игр +10
 x6. Анимации или звуки, или настройки игры. Баллы начисляются за любой из перечисленных пунктов +10
 x7. Очень высокое качество оформления приложения и/или дополнительный не предусмотренный в задании функционал, улучшающий качество приложения +10
@@ -27,25 +27,49 @@ function arrangeBoard(size) {
 arrangeBoard(3);
 
 let moves = 0; //moves in game
+let crossCells = [], roundCells = [];
+
+const winCells = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+const checkGameOver = () => {
+  if (winCells.some(win => win.every(el => crossCells.includes(el)))) {
+    showGameOver('X');
+  } else if (winCells.some(win => win.every(el => roundCells.includes(el)))) {
+    showGameOver('O');
+  } else if (moves === 8) {
+    showGameOver('No one');
+  }
+}
 
 const makeMove = (event) => {
   const toe = (moves % 2) ? 'round' : 'cross';
   event.target.insertAdjacentHTML('beforeend', `<img class="img-cell" src="assets/svg/${toe}.svg" alt="${toe}">`);
+  (moves % 2) ? roundCells.push(+event.target.dataset.cell) : crossCells.push(+event.target.dataset.cell);
+  checkGameOver();
   moves++;
 }
 
 gameBoard.addEventListener('click', makeMove);
 
 /* Local Storage */
-let recordTable = []; //array of 10 last games for storage in localStorage
+let recordTableTTT = []; //array of 10 last games for storage in localStorage
 
-const setLocalStorage = () => localStorage.setItem('records', JSON.stringify(recordTable));
+const setLocalStorage = () => localStorage.setItem('records', JSON.stringify(recordTableTTT));
 
 const getLocalStorage = () => {
   if (localStorage.getItem('records')) {
-    recordTable = JSON.parse(localStorage.getItem('records'));
+    recordTableTTT = JSON.parse(localStorage.getItem('records'));
   }
-  fillRecordTable(recordTable);
+  fillRecordTable(recordTableTTT);
 }
 
 window.addEventListener('load', getLocalStorage);
@@ -55,10 +79,10 @@ const gameOver = document.querySelector('.game-over');
 const showModal = () => gameOver.classList.add('show');
 
 //write result of the game to Array to be placed in Local Storage
-const writeRecord = () => {
-  recordTable.push(moves);
-  if (recordTable.length > 10) {
-    recordTable.shift();
+const writeRecord = (res) => {
+  recordTableTTT.push(res);
+  if (recordTableTTT.length > 10) {
+    recordTableTTT.shift();
   }
 }
 
@@ -67,7 +91,7 @@ const divRecordTable = document.querySelector('.record-table');
 //Fill Table of records with records from localStorage
 const fillRecordTable = table => {
   for (let record of table) {
-    const pRecord = `<p class="record-line">${record} moves</p>`;
+    const pRecord = `<p class="record-line">${record}</p>`;
     divRecordTable.insertAdjacentHTML('beforeend', pRecord);
   }
 }
@@ -84,13 +108,13 @@ const toggleOpen = () => {
 tableRecordsButton.addEventListener('click', toggleOpen);
 
 /* Show modal window when found all the pairs*/
-const showGameOver = () => {
+const showGameOver = res => {
 //  document.title += ` - ${moves} moves`;
   const okButton = gameOver.querySelector('.ok');
   const pMoves = gameOver.querySelector('.moves');
-  pMoves.textContent = `You make ${moves} moves`;
+  pMoves.textContent = `${res} won in ${moves+1} moves`;
   showModal();
-  writeRecord();
+  writeRecord(pMoves.textContent);
   okButton.addEventListener('click', newGame);
 }
 
